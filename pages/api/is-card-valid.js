b16931c27380c94c9cbc29b1c6993d1250b4fd71
emremutlu08 <mutluemre93@gmail.com>
2023-08-15 22:49:03 +0300

    fix: fixed card checker

--- pages/api/is-card-valid.js
+++ pages/api/is-card-valid.js
@@ -5,9 +5,15 @@ export default async function isCardValid(req, res) {
     `https://mocki.io/v1/a5ae8585-b42d-486b-a4ff-25ebfebbaddf`
   );
   const result = await response.json();
-  const isAnyCardMatch = result.some(
-    (item) => JSON.stringify(item) === req.body
-  );
+  const body = JSON.parse(req?.body);
+
+  const isAnyCardMatch = result.some((item) => {
+    return (
+      item?.number === body?.number &&
+      item?.ccv === body?.ccv &&
+      item?.exp === body?.exp
+    );
+  });
 
   res.status(200).json({ isValid: isAnyCardMatch });
 }
